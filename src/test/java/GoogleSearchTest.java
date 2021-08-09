37799613f43579be633b159f5f094c598483cad0
Tatevar <42369756+Tatevar@users.noreply.github.com>
2021-08-09 21:43:55 +0400

    Update GoogleSearchTest.java
    A few test were added

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -1,17 +1,47 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleSearchTest {
     @Test
-    public void test1() throws InterruptedException {
+    public void test1() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.manage().window().fullscreen();
+        driver.get("https://www.google.by/");
         driver.findElement(By.name("q")).sendKeys("Hello world", Keys.ENTER);
-        Thread.sleep(5000);
         driver.quit();
+    }
 
+    @Test
+    public void test2() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        driver.findElement(By.name("q")).sendKeys("//", Keys.ENTER);
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+        driver.quit();
+    }
+
+    @Test
+    public void test3() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        driver.findElement(By.name("q")).sendKeys("Hello world", Keys.ENTER);
+        driver.findElement(By.xpath("(//h3)[1]")).click();
+        Assert.assertEquals(driver.findElement(By.tagName("span")).getText(), "History");
+        driver.quit();
+    }
+
+    @Test
+    public void test4() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        driver.findElement(By.name("q")).sendKeys("hello world", Keys.ENTER);
+        WebElement element = driver.findElement(By.xpath("(//h3)[3]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        Thread.sleep(500);
+        element.click();
+        driver.quit();
     }
 }
