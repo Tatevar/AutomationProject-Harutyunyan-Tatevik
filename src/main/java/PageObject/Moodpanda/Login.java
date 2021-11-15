1a89d61996a70c58b9f5d5240d82012310fc01eb
Tatevar <42369756+Tatevar@users.noreply.github.com>
2021-11-15 16:52:45 +0300

    t

--- src/main/java/PageObject/Moodpanda/Login.java
+++ src/main/java/PageObject/Moodpanda/Login.java
@@ -1,14 +1,10 @@
 package PageObject.Moodpanda;
 
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.conditions.MatchText;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 
 import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.switchTo;
 
 public class Login {
     SelenideElement email = $(byXpath("//*[@name='ctl00$ContentPlaceHolderContent$TextBoxEmail']"));
